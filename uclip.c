54f0757aee6ae8630f964b8bb5fb97bc91897805
Logan Ellis <logan_ellis@me.com>
2022-06-20 02:00:41 -0500

    Add permission checking and remove quiet flag

--- uclip.c
+++ uclip.c
@@ -1,4 +1,5 @@
 #include <sys/errno.h>
+#include <sys/stat.h>
 
 #include <err.h>
 #include <fcntl.h>
@@ -12,10 +13,11 @@
 
 #define BUFFER_SIZE 4096
 #define MAX_FNAME_SIZE 512
-
-static bool quiet = false;
+#define CLIPPERMS (S_IWUSR|S_IRUSR)
 
 bool stdin_empty(void);
+void check_permissions(char *path);
+void create_clipboard(char *path);
 void get_clipboard_path(char *);
 FILE *open_clipboard(char *, char *);
 int read_clipboard(FILE *);
@@ -45,6 +47,55 @@ stdin_empty(void)
 }
 
 /*
+ * Ensure that clipboard mode is user read/write only. Will also attempt to 
+ * create if it does not exist.
+ */
+void
+check_permissions(char *path)
+{
+	struct stat file_stats;
+	mode_t file_permissions;
+
+	if (stat(path, &file_stats) < 0) {
+		if (errno == ENOENT) {
+			/* File does not exist, so let's create it */
+			create_clipboard(path);
+		} else {
+			err(EX_UNAVAILABLE, "Unable to verify clipboard permissions");
+		}
+	} else {
+    /* Mask out all except permissions */
+    file_permissions = file_stats.st_mode & ALLPERMS;
+
+    if (file_permissions != CLIPPERMS) {
+      warnx("Incorrect clipboard permissions. Setting to user rw only");
+      chmod(path, CLIPPERMS);
+    }
+	}
+}
+
+/*
+ * Create the clipboard with the appropriate permissions.
+ */
+void
+create_clipboard(char *path)
+{
+	int fd;
+
+	if ((fd = open(path, O_RDWR | O_CREAT)) < 0) {
+		err(EX_UNAVAILABLE, "Unable to create clipboard at %s", path);
+	}
+
+	if (fchmod(fd, CLIPPERMS) < 0) {
+		err(EX_UNAVAILABLE, "Unable to set clipboard permissions");
+	}
+
+  if(close(fd)){
+    err(EX_UNAVAILABLE, "Error closing clipboard");
+  }
+
+}
+/*
  * Validates the clipboard path and writes it to `clipboard_dir`. Should a
  * failure occur, the function will terminate the process without returning.
  */
@@ -56,15 +107,13 @@ get_clipboard_path(char *clipboard_dir)
 	bytes_written = snprintf(clipboard_dir, MAX_FNAME_SIZE - 1,
 	    "/tmp/uclip-%i", getuid());
 
-	if (!quiet) {
-		if (bytes_written >= MAX_FNAME_SIZE) {
-			errx(EX_USAGE,
-			    "Failed to write clipboard name. "
-			    "Resultant string exceeds %i",
-			    MAX_FNAME_SIZE);
-		} else if (bytes_written < 0) {
-			err(EX_UNAVAILABLE, "Error writing clipboard name");
-		}
+	if (bytes_written >= MAX_FNAME_SIZE) {
+		errx(EX_USAGE,
+		    "Failed to write clipboard name. "
+		    "Resultant string exceeds %i",
+		    MAX_FNAME_SIZE);
+	} else if (bytes_written < 0) {
+		err(EX_UNAVAILABLE, "Error writing clipboard name");
 	}
 }
 
@@ -74,7 +123,7 @@ get_clipboard_path(char *clipboard_dir)
 void
 usage(void)
 {
-	fprintf(stderr, "usage: %s [-q] -i/-o\n", getprogname());
+	fprintf(stderr, "usage: %s -i/-o\n", getprogname());
 	exit(EX_USAGE);
 }
 
@@ -88,9 +137,10 @@ open_clipboard(char *clipboard_path, char *mode)
 	FILE *clipboard;
 	clipboard = fopen(clipboard_path, mode);
 
-	if (clipboard == NULL && !quiet) {
+	if (clipboard == NULL) {
 		err(EX_UNAVAILABLE, "Error opening clipboard at %s", clipboard_path);
 	}
+
 	return clipboard;
 }
 
@@ -122,9 +172,6 @@ parse_args(char *mode, int argc, char *argv[])
 			strcpy(mode, "r");
 			mode_set = true;
 			break;
-		case 'q':
-			quiet = true;
-			break;
 		case '?':
 			/* FALLTHROUGH */
 		default:
@@ -154,14 +201,13 @@ read_clipboard(FILE * clipboard)
 	while (bytes_read >= BUFFER_SIZE) {
 		bytes_read = fread(buffer, 1, BUFFER_SIZE, clipboard);
 
-		if (fwrite(buffer, 1, bytes_read, stdout) < bytes_read && !quiet) {
+		if (fwrite(buffer, 1, bytes_read, stdout) < bytes_read) {
 			err(EX_UNAVAILABLE, "Error writing to stdout");
-		};
-
+		}
 		total_bytes += bytes_read;
 	}
 
-	if (ferror(stdin) && !quiet) {
+	if (ferror(stdin)) {
 		err(EX_UNAVAILABLE, "Error reading clipboard");
 	}
 	return total_bytes;
@@ -190,7 +236,7 @@ write_clipboard(FILE * clipboard)
 		total_bytes += bytes_read;
 	}
 
-	if (ferror(stdin) && !quiet) {
+	if (ferror(stdin)) {
 		err(EX_UNAVAILABLE, "Error writing clipboard");
 	}
 	return total_bytes;
@@ -209,6 +255,7 @@ main(int argc, char *argv[])
 
 	parse_args(mode, argc, argv);
 	get_clipboard_path(clipboard_path);
+	check_permissions(clipboard_path);
 	clipboard = open_clipboard(clipboard_path, mode);
 
 	if (*mode == 'r') {
